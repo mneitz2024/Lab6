415fcc02f564a480dbc31f1da69175bf25dbb00c
mneitz2024 <m.neitz@ufl.edu>
2023-03-22 15:01:00 -0400

    Wrote main menu function

--- password__encoder.py
+++ password__encoder.py
@@ -8,3 +8,24 @@ def encoder(string):  # function to encode passwords
         y = str(x)
         encoded_string += y
     return encoded_string
+
+
+if __name__ == '__main__':
+    condition = True
+    while condition is True:
+        print('Menu')
+        print('------')
+        print('1. Encode')
+        print('2. Decode')
+        print('3. Quit')
+        print('')
+        print('')
+
+        option = int(input('Please enter an option:'))
+
+        if option == 1:  # takes option variable and uses in for loop to encode, decode, or quit the program
+            password = input('Please enter your password to encode:')
+            encoded_password = encoder(password)
+            print('Your password has been encoded and stored!')
+        if option == 3:
+            condition = False
